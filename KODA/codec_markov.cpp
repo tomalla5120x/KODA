7f878288c79ed261f3cdb3aa3de04ade193aef69
bedzkowp <p.bedzkowski@gmail.com>
2018-05-19 00:48:13 +0200

    model Markova

--- KODA/codec_markov.cpp
+++ KODA/codec_markov.cpp
@@ -2,38 +2,96 @@
 #define CODEC_MARKOV_H
 
 #include "codec_markov.h"
-
+#include "tree.h"
+#include <deque>
 using namespace std;
+
+#define MAX_SYMBOL_INDEX 255
 
 Result<CompressionStats> compress_markov(const unsigned char* inDataBuffer, unsigned int bufferSize, ostream& out)
 {
-	// STUB: (brak kompresji)
+	vector<vector<unsigned int>> bitLengths(256);
+	vector<unsigned int> symbolCount(256, 0);
+	deque<HuffmanTreeEncoder> encoder;
+	deque<HuffmanTreeBuilder> tree;
+	unsigned char currentSymbol;
+	unsigned char nextSymbol;
+	unsigned int sumBitLen;
 
-	for(unsigned int i = 0; i < bufferSize; ++i)
-		out.put(inDataBuffer[i]);
+	for (unsigned int i = 0; i < 256; ++i) {
+		tree.emplace_back(MAX_SYMBOL_INDEX);
+	}
+
+	for (unsigned int i = 0; i < bufferSize - 1; ++i) {
+		currentSymbol = inDataBuffer[i];
+		nextSymbol = inDataBuffer[i + 1];
+		tree[currentSymbol].addSymbol(nextSymbol);
+	}
+
+	for (unsigned int i = 0; i < 256; ++i) {
+		tree[i].getBitLengths(bitLengths[i]);
+		for (unsigned int j = 0; j < 256; ++j) {
+			out.put(bitLengths[i][j]);
+		}
+		encoder.emplace_back(bitLengths[i]);
+	}
+	
+	out.put(inDataBuffer[0]);
+	sumBitLen = 8;
+	BitStreamWriter bitStreamManager(out);
+	for (unsigned int i = 0; i < bufferSize - 1; ++i) {
+		currentSymbol = inDataBuffer[i];
+		nextSymbol = inDataBuffer[i + 1];
+		Code code = encoder[currentSymbol].getCode((unsigned int)nextSymbol);
+		bitStreamManager.addCode(code);
+		symbolCount[currentSymbol]++;
+		sumBitLen += bitLengths[currentSymbol][nextSymbol];
+	}
+	symbolCount[nextSymbol]++;
+	bitStreamManager.finish();
 
 	CompressionStats stats;
-	stats.entropy = 3.4;
-	stats.meanBitLength = 3.65;
-	stats.headerSize = 23;
+	stats.entropy = 0.0;
+
+	for (unsigned int i = 0; i < 256; ++i) {
+		stats.entropy += 1.0 * symbolCount[i] / bufferSize * tree[i].getEntropy();
+	}
+	
+	stats.meanBitLength = 1.0 * sumBitLen / bufferSize;
+	stats.headerSize = 65536;
 	return{ true, stats };
 }
 
 bool decompress_markov(istream& in, unsigned char* outDataBuffer, unsigned int bufferSize)
 {
-	// STUB: (brak kompresji)
+	vector<vector<unsigned int>> bitLengths(256);
+	deque<HuffmanTreeDecoder> decoder;
 
-	for(unsigned int i = 0; i < bufferSize; ++i)
-	{
-		unsigned char byte = in.get();
+	for (unsigned i = 0; i < 256; ++i) {
+		for (unsigned int j = 0; j < 256; ++j) {
+			unsigned char byte = in.get();
+			if (byte == EOF) {
+				cerr << "Unexpected end of file." << endl;
+				return false;
+			}
+			bitLengths[i].push_back((unsigned int)byte);
+		}
+		decoder.emplace_back(bitLengths[i]);
+	}
 
-		if(byte == EOF)
-		{
-			cerr << "Unexpected end of file." << endl;
+	unsigned char currentSymbol = in.get();
+	BitStreamReader bitStreamReader(in);
+	outDataBuffer[0] = currentSymbol;
+
+	for (unsigned int i = 1; i < bufferSize; ++i) {
+		try {
+			currentSymbol = (unsigned char)bitStreamReader.nextSymbol(decoder[currentSymbol]);
+			outDataBuffer[i] = currentSymbol;
+		}
+		catch (runtime_error& e){
+			cerr << e.what() << endl;
 			return false;
 		}
-
-		outDataBuffer[i] = byte;
 	}
 
 	return true;
--- KODA/codec_markov.h
+++ KODA/codec_markov.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "codec.h"
 #include "utils.h"
+#include "bits_managers.h"
 
 Result<CompressionStats> compress_markov(const unsigned char* inDataBuffer, unsigned int bufferSize, std::ostream& out);
 bool decompress_markov(std::istream& in, unsigned char* outDataBuffer, unsigned int bufferSize);
